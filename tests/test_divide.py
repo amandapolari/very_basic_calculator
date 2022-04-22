b8388a41959d3cb5f176c470fcd08d79cc13e17f
luanleonardo <luan.moraes@loggi.com>
2022-04-22 18:01:58 -0400

    fix float return

--- tests/test_divide.py
+++ tests/test_divide.py
@@ -4,7 +4,7 @@ from very_basic_calculator import divide
 
 @pytest.mark.parametrize("x, y", [(1, 1), (4, 1), (2, 9), (4, 2)])
 def test_divide(x, y):
-    assert divide(x, y) == x / y
+    assert divide(x, y) == x // y
 
 
 @pytest.mark.parametrize("x, y", [(9, 0)])
