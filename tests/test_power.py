e2e5c316cd2f23e742667282d42a09252a58f8f9
Amanda <amandaoliveirapolari@gmail.com>
2022-04-30 12:05:27 -0400

    add power test

---
+++ tests/test_power.py
@@ -0,0 +1,7 @@
+import pytest
+from very_basic_calculator import power
+
+
+@pytest.mark.parametrize("x, y", [(1, 1), (4, 1), (2, 9), (4, 2), (9, 0)])
+def test_power(x, y):
+    assert power(x, y) == x ** y
