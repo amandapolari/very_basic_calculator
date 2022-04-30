eebcf4aae8802d80fedff36f6db0c1ad7d8646e5
luanleonardo <luan.moraes@loggi.com>
2022-04-30 08:40:40 -0400

    noqa F401

--- very_basic_calculator/__init__.py
+++ very_basic_calculator/__init__.py
@@ -1,6 +1,6 @@
 __version__ = "0.1.0"
 
-from very_basic_calculator.add import add
-from very_basic_calculator.subtract import subtract
-from very_basic_calculator.multiply import multiply
-from very_basic_calculator.divide import divide
+from very_basic_calculator.add import add  # noqa F401
+from very_basic_calculator.subtract import subtract  # noqa F401
+from very_basic_calculator.multiply import multiply  # noqa F401
+from very_basic_calculator.divide import divide  # noqa F401
